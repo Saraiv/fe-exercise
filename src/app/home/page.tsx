70dcf4800f1077bf63dab2769493ec11df47a1ea
Saraiv <mainsaraiva@gmail.com>
2024-08-16 14:15:51 +0100

    Quick adjust on css

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,27 +14,29 @@ const Home = () => {
         return (
             <div className="block bg-stone-700 text-center w-2/3 justify-center mx-auto my-10 p-10 rounded-md">
                 <h1 className="text-white text-2xl mb-5">All Posts</h1>
-                {json_data.posts.map((post, index) => {
-                    // Find the user who made the post
-                    const user = json_data.users.find(user => user.id === post.userId)
+                {
+                    json_data.posts.map((post, index) => {
+                        const user = json_data.users.find(user => user.id === post.userId)
 
-                    return (
-                        <div key={index} className="text-left mb-5 p-5">
-                            <h2 className="text-xl text-blue-300">{post.title}</h2>
-                            <p><strong>User:</strong> {user ? `${user.firstName} ${user.lastName}` : "Unknown User"}</p>
-                            <p><strong>Posted At:</strong> {new Date(post.postedAt).toLocaleString()}</p>
-                            <p><strong>Content:</strong> {post.text}</p>
-                        </div>
-                    )
-                })}
+                        return (
+                            <div key={index} className="text-left mb-5 p-5">
+                                <h2 className="text-xl text-blue-300">{post.title}</h2>
+                                <p><strong>User:</strong> {user ? `${user.firstName} ${user.lastName}` : "Unknown User"}</p>
+                                <p><strong>Posted At:</strong> {new Date(post.postedAt).toLocaleString()}</p>
+                                <p><strong>Content:</strong> {post.text}</p>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
     else {
         return (
-            <div>
-                <h1>You need to create an account first!</h1>
-                <Link href='/register'>Create your account</Link>
+            <div className="w-full text-center">
+                <h1 className="block">You need to create an account first!</h1>
+                <Link className="block" href="/register">Create your account</Link>
+                <Link className="block" href="/login">Login!</Link>
             </div>
         )
     }
